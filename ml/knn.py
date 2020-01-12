b946c93118cb91e5abe332990aa076af017c7c07
mfy-royce <mfy-111@163.com>
2020-01-12 19:26:57 +0800

    first ml KNN

---
+++ ml/knn.py
@@ -0,0 +1,14 @@
+# ! /usr/bin/python
+# encoding: utf-8
+
+"""
+author:Royce
+contact:mfy-111@163.com
+@file: knn.py
+@time:  18:50
+@welcom to learn ai
+"""
+
+class KNN(object):
+    def __init__(self):
+        pass
